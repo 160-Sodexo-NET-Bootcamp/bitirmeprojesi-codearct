b189a681de70060bf5d9dbda1d63846171945783
codearct <codearct@gmail.com>
2022-02-08 21:18:43 +0300

    Work autorization and authentication

---
+++ ByCell/Core/Security/Hashing/HashingHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Security.Cryptography;
+
+namespace Core.Security.Hashing
+{
+    public class HashingHelper
+    {
+        //Kullanıcının kayıt sırasında verdiği şifreye bağlı olarak password hash ve password salt üretecek
+        //ürettiği hash ve saltı dışarı verecek
+        public static void CreatePasswordHash(string registeredPassword,out byte[] registeredPasswordHash,out byte[] registeredPasswordSalt)
+        {
+            using (var hmac = new HMACSHA256())
+            {
+                registeredPasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registeredPassword));
+                registeredPasswordSalt = hmac.Key;
+            }
+        }
+        //kullanıcı giriş sırasında girdiği şifreye ve veritabanından gelen salta bağlı olarak yeni bir password hash üretecek
+        //üretilen yeni password hash ile veritabanındaki password hashi karşılaştıracak
+        //birebir eşleşme sağlanırsa true; sağlanmazsa false dönecek
+        public static bool CheckPasswordHash(string loginPassword, byte[] registeredPasswordHash, byte[] registeredPasswordSalt)
+        {
+            using (var hmac = new HMACSHA256(registeredPasswordSalt))
+            {
+                var loginPasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginPassword));
+
+                for (int i = 0; i < loginPasswordHash.Length; i++)
+                {
+                    if (loginPasswordHash[i]!= registeredPasswordHash[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+
+        }
+    }
+}
---
+++ ByCell/Core/Security/JWT/TokenHelper.cs
@@ -0,0 +1,60 @@
+﻿using Core.Entities.Concrete;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Security.JWT
+{
+    public class TokenHelper : ITokenHelper
+    {
+
+        public IConfiguration Configuration { get; }
+        private TokenConfig _tokenConfig;
+        private DateTime _accessTokenExpiration;
+        public TokenHelper(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            _tokenConfig = Configuration.GetSection("TokenConfig").Get<TokenConfig>();
+
+        }
+        public AccessToken CreateToken(User user)
+        {
+            _accessTokenExpiration = DateTime.Now.AddMinutes(_tokenConfig.AccessTokenExpiration);
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_tokenConfig.SecurityKey));
+            var signingCredentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var securityToken = CreateJwtSecurityToken(_tokenConfig, user, signingCredentials);
+            var jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+            var token = jwtSecurityTokenHandler.WriteToken(securityToken);
+
+            return new AccessToken
+            {
+                Token = token,
+                Expiration = _accessTokenExpiration,
+                RefreshToken= CreateRefreshToken()
+            };
+
+        }
+
+        public JwtSecurityToken CreateJwtSecurityToken(TokenConfig tokenConfig, User user,
+            SigningCredentials signingCredentials)
+        {
+            var securityToken = new JwtSecurityToken(
+                issuer: tokenConfig.Issuer,
+                audience: tokenConfig.Audience,
+                expires: _accessTokenExpiration,
+                notBefore: DateTime.Now,
+                signingCredentials: signingCredentials
+            );
+            return securityToken;
+        }
+        public string CreateRefreshToken()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
